71b995ae9d717ac5e0c48164d66736d4ae344eea
Vasilii <bespalchuk1985@gmail.com>
2022-05-24 16:41:27 +0300

    Ex01

--- Ex01/Program.cs
+++ Ex01/Program.cs
@@ -1,16 +1,16 @@
-﻿/*Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
-M = 1; N = 15 -> 120*/
-int SumNumbers(int numberM, int numberN, int sum = 0)
-{
-    if (numberM <= numberN)
-    {
-        sum += numberM;
-        sum = SumNumbers(++numberM, numberN, sum);
-    }
-     return sum;
-}
+﻿//Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.//
+Console.WriteLine("Задайте число N: ");
+ int n = Convert.ToInt32(Console.ReadLine());
 
-int numM = 1;
-int numN = 15;
-int sumNumbers = SumNumbers(numM, numN);
-Console.WriteLine($"Сумма натуральных элементов от {numM} до {numN} равна {sumNumbers}");
+ int result = Rekurs(n);
+ Console.WriteLine(result);
+
+ int Rekurs(int number)
+ {  
+    if (number == 1)
+    {
+        return number; 
+    }
+    Console.Write(number + ", ");
+    return Rekurs (number - 1);
+ }
