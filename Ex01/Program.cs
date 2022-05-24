39c251fb282e79de3c7038fa9bce4019dc7945aa
Vasilii <bespalchuk1985@gmail.com>
2022-05-24 15:24:19 +0300

    Ex01

---
+++ Ex01/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120*/
+int SumNumbers(int numberM, int numberN, int sum = 0)
+{
+    if (numberM <= numberN)
+    {
+        sum += numberM;
+        sum = SumNumbers(++numberM, numberN, sum);
+    }
+     return sum;
+}
+
+int numM = 1;
+int numN = 15;
+int sumNumbers = SumNumbers(numM, numN);
+Console.WriteLine($"Сумма натуральных элементов от {numM} до {numN} равна {sumNumbers}");
